0c0f9e29e134a8f1c119b3b360852007f5f798b8
Akifumi <kusacurrency@gmail.com>
2019-10-07 13:33:42 +0400

    add netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -151,5 +151,6 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    'gatsby-plugin-netlify'
   ],
 }
