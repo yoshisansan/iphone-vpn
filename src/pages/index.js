ccfb692617716610b60ce8b4528736208b6f918e
Akifumi <kusacurrency@gmail.com>
2019-10-07 13:51:05 +0400

    adjust index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,20 @@
 import React from "react"
-// import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-// import Image from "../components/image"
-// import SEO from "../components/seo"
 import {graphql, StaticQuery} from 'gatsby'
 
 const IndexPage = () => (
   <Layout>
     <StaticQuery query={graphql`
-    {
-      allWordpressPost{
-        edges {
-          node{
-            id
-            title
-            content
-          }
-        }
+      {
+        allWordpressPage {
+          edges {
+            node{
+              title
+              content
+            }
+           }
+         }
       }
-     }
-   
     `} render={props => (
       <div>
         {props.allWordpressPage.edges.map(page => (
@@ -28,11 +22,11 @@ const IndexPage = () => (
             <h1>
               {page.node.title}
             </h1>
-            <div dangerouslySetInnerHTML={{__html: page.node.content}} />
+            <div dangerouslySetInnerHTML={{__html: page.node.content}}/>
           </div>
         ))}
       </div>
-    )}/>
+    )} />
   </Layout>
 )
 
